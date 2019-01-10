9e705bb58339bc8e1516dfb3998f283029c0c94d
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2019-01-10 22:31:24 +0200

    Create AP_1.ino

---
+++ 1/AP_1.ino
@@ -0,0 +1,43 @@
+/*  Accesspoint - station communication without router
+ *  see: https://github.com/esp8266/Arduino/blob/master/doc/esp8266wifi/soft-access-point-class.rst
+ *       https://github.com/esp8266/Arduino/blob/master/doc/esp8266wifi/soft-access-point-examples.rst
+ *       https://github.com/esp8266/Arduino/issues/504
+ *  Works with: ST_1.ino 
+ *  small changes by Nicu FLORICA (niq_ro)
+ */ 
+
+#include <ESP8266WiFi.h>
+
+WiFiServer server(80);
+IPAddress IP(192,168,4,15);
+IPAddress mask = (255, 255, 255, 0);
+
+byte ledPin = 2;
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP("Wemos_AP", "Wemos_comm");
+  WiFi.softAPConfig(IP, IP, mask);
+  server.begin();
+  pinMode(ledPin, OUTPUT);
+  Serial.println();
+  Serial.println("accesspoint_bare_01a.ino");
+  Serial.println("Server started.");
+  Serial.print("IP: ");     Serial.println(WiFi.softAPIP());
+  Serial.print("MAC:");     Serial.println(WiFi.softAPmacAddress());
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {return;}
+  digitalWrite(ledPin, LOW);
+  String request = client.readStringUntil('\r');
+  Serial.println("********************************");
+  Serial.println("From the station: " + request);
+  client.flush();
+  Serial.print("Byte sent to the station: ");
+  Serial.println(client.println(request + "ca" + "\r"));
+//  Serial.println(client.println("Hi, friend" + "\r"));
+  digitalWrite(ledPin, HIGH);
+}
